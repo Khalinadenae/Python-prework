808579ef044ded34ef08ccb5119d27a4ee6283a2
Khalinadenae <96637309+Khalinadenae@users.noreply.github.com>
2022-04-20 19:33:47 -0400

    created function to return odd numbers 1-100

--- prework.py
+++ prework.py
@@ -12,12 +12,14 @@ hello_name("kdb033")
 #Question 2
 #Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
 
- 
- #def first_odds():
-    
-                
-  
+def first_odds():
+    for i in range(1,101):
+        if i % 2 != 0:
+            print(i)
+            continue
 
+first_odds()
+        
 #Question 3
 #Please write a Python function, max_num_in_list to return the max number of a given list. The first line of the code has been defined as below.
 
