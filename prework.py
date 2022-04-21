2892e80dd80935a0db50bb121f57b9f970f9ece5
Khalinadenae <96637309+Khalinadenae@users.noreply.github.com>
2022-04-20 20:15:08 -0400

    function to check if year is a leap year

--- prework.py
+++ prework.py
@@ -35,9 +35,16 @@ max_num_in_list(numbers)
 #Question 4
 #Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100, unless it is also divisible by 400. The return should be boolean Type (true/false).
 
-   # def is_leap_year(a_year):
-   
 
+def is_leap_year(a_year):
+#leap year is divisible by 4, but not divisible by 100 unless it is divisible by 400
+    if (a_year % 4 == 0) and (a_year % 100 != 0) or (a_year % 400 == 0) :
+     return True 
+    else: 
+     return False 
+    
+is_leap_year(1988)
+               
 #Question 5
 #Write a function to check to see if all numbers in list are consecutive numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
 
